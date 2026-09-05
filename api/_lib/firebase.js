57816c769f799887198dc1a0951e1770a646d898
unikmo <mbanwie@googlemail.com>
2026-09-05 10:51:55 +0200

    Fix Firebase OAuth JWT grant type

--- api/_lib/firebase.js
+++ api/_lib/firebase.js
@@ -101,7 +101,7 @@ async function getAccessToken() {
     method: 'POST',
     headers: { 'content-type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams({
-      grant_type: 'urn:ietf:params:oauth2:grant-type:jwt-bearer',
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
       assertion,
     }),
   });
